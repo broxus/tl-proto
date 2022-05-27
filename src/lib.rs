27153df64cd6e08276eb44003a628dd9dc1ca66d
Ivan Kalinin <i.kalinin@dexpa.io>
2022-05-27 19:10:28 +0200

    Add `_as_boxed` and `hash`  methods

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,15 @@ where
     T::read_from(packet, &mut 0)
 }
 
+#[inline(always)]
+pub fn deserialize_as_boxed<'a, T>(packet: &'a [u8]) -> TlResult<T>
+where
+    T: TlRead<'a> + BoxedConstructor,
+{
+    let BoxedReader(result) = deserialize(packet)?;
+    Ok(result)
+}
+
 pub fn serialize<T>(data: T) -> Vec<u8>
 where
     T: TlWrite,
@@ -30,6 +39,14 @@ where
     result
 }
 
+#[inline(always)]
+pub fn serialize_as_boxed<T>(data: T) -> Vec<u8>
+where
+    T: TlWrite + BoxedConstructor,
+{
+    serialize(data.into_boxed_writer())
+}
+
 pub fn serialize_into<T>(data: T, buffer: &mut Vec<u8>)
 where
     T: TlWrite,
@@ -37,4 +54,33 @@ where
     buffer.clear();
     buffer.reserve(data.max_size_hint());
     data.write_to(buffer);
+}
+
+#[inline(always)]
+pub fn serialize_into_as_boxed<T>(data: T, buffer: &mut Vec<u8>)
+where
+    T: TlWrite + BoxedConstructor,
+{
+    serialize_into(data.into_boxed_writer(), buffer);
+}
+
+#[cfg(feature = "hash")]
+pub fn hash<T>(data: T) -> digest::Output<sha2::Sha256>
+where
+    T: TlWrite,
+{
+    use digest::Digest;
+
+    let mut hasher = sha2::Sha256::new();
+    HashWrapper(data).update_hasher(&mut hasher);
+    hasher.finalize()
+}
+
+#[cfg(feature = "hash")]
+#[inline(always)]
+pub fn hash_as_boxed<T>(data: T) -> digest::Output<sha2::Sha256>
+where
+    T: TlWrite + BoxedConstructor,
+{
+    hash(data.into_boxed_writer())
 }
